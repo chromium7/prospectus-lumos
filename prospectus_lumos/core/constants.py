e590fd21ab14127e476be6d4fceda1efdb460d47
Christofer Saputra <61144981+chromium7@users.noreply.github.com>
2025-08-30 19:35:32 +0700

    Add dark mode (#76)
    * Add dark mode
    
    * Fix dark mode
    
    * Update feedback
    
    ---------
    
    Co-authored-by: chromium7 <christofer@ui.co.id>

---
+++ prospectus_lumos/core/constants.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from django.db.models import IntegerChoices
+
+
+class Months(IntegerChoices):
+    JANUARY = 1, "January"
+    FEBRUARY = 2, "February"
+    MARCH = 3, "March"
+    APRIL = 4, "April"
+    MAY = 5, "May"
+    JUNE = 6, "June"
+    JULY = 7, "July"
+    AUGUST = 8, "August"
+    SEPTEMBER = 9, "September"
+    OCTOBER = 10, "October"
+    NOVEMBER = 11, "November"
+    DECEMBER = 12, "December"
+
+
+MONTHS_LIST: list[tuple[int, str]] = list(Months.choices)
